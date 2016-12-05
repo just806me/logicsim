ac0b71683e823b180d654cd930a7b4701774c9bd
Just Me <just806me@gmail.com>
2016-12-05 16:25:20 +0200

    add error logging

--- LogicSimulator.WindowsUI/Program.cs
+++ LogicSimulator.WindowsUI/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
+using System.IO;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LogicSimulator.WindowsUI
@@ -13,15 +17,36 @@ namespace LogicSimulator.WindowsUI
         [STAThread]
         static void Main(string[] args)
         {
+#if DEBUG
+            var dom = AppDomain.CurrentDomain;
+            dom.FirstChanceException += (sender, e) =>
+            {
+                var builder = new StringBuilder();
+
+                builder.AppendLine($"UTC time: {DateTime.UtcNow.ToString()}");
+                builder.AppendLine($"Error: {e.Exception.Message}");
+                builder.AppendLine($"Short stack: {e.Exception.StackTrace}");
+                builder.AppendLine("Full stack:");
+                foreach (var frame in new StackTrace(true).GetFrames())
+                    builder.Append(frame.ToString());
+                builder.AppendLine("//-------------------------------------------//");
+
+                File.AppendAllText("log.txt", builder.ToString());
+            };
+#endif
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-			//ThreadLocalization
-			var ci = new CultureInfo("uk");
-			Thread.CurrentThread.CurrentUICulture = ci;
-			Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
+            //ThreadLocalization
+            var ci = new CultureInfo("uk");
+            Thread.CurrentThread.CurrentUICulture = ci;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
 
-			Application.Run(new MainForm(args.Length > 0 ? args[0] : null));
+            Trace.Listeners.Add(new TextWriterTraceListener("trace.log"));
+            Trace.AutoFlush = true;
+
+            Application.Run(new MainForm(args.Length > 0 ? args[0] : null));
         }
     }
 }
